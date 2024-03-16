65d3af3c725216d1572e03c505122353c4316e10
Anatoly Ometov <Fun_spawn@mail.ru>
2024-03-16 20:54:40 +0400

    added first program

--- Lesson01/Program.cs
+++ Lesson01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+System.Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 7 == 0 && number % 23 == 0)
+{
+   System.Console.WriteLine("Данное число одновременно кратно 7 и 23!");
+}
+else
+{
+   System.Console.WriteLine("Данное число не кратно 7 и 23!"); 
+}
